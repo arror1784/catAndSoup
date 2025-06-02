66a032ee38b1158740fae53f3b648b42fef2c518
arror <arror@DESKTOP-ES8CCLH>
2025-06-02 20:24:55 +0900

    초기 커밋.

---
+++ catAndSoup/catAndSoup.cpp
@@ -0,0 +1,245 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <Windows.h>
+
+#define ROOM_WIDTH          10
+#define HME_POS             1
+#define BWL_PO              (ROOM_WIDTH - 2)
+
+#define INTIMACY_MIN        0
+#define INTIMACY_DEFAULT    2
+#define INTIMACY_MAX        4
+
+#define POTATO_SOUP         1
+#define MUSHROOM_SOUP       2
+#define BROCCOLI_SOUP       3
+
+#define ACTION_NOTHING       0
+#define ACTION_SCRATCH      1
+
+#define DELAY_INTRO 1000
+#define DELAY_TURN 2500
+
+void reset(int milliSec);
+void mSleep(int milliSec);
+void introAndGetName(char* catName, int catNameLength);
+void showStatus(int intimacy, int soupCount);
+int getAction(); //return 0 or 1 for valid input, return -1 for invalid input
+int rollDice(); // return 1~6
+void showRoom(int catPos, int catPreviousPos);
+int checkCatPositionAndMakeSoup(int catPos); // return -1 is nothing, return 0 is home, return 1,2,3 is potato, mushroom, broccoli
+
+int main()
+{
+    char catName[100] = { 0 };
+    int intimacy = INTIMACY_DEFAULT;
+    int soupCount = 0;
+    int catPos = HME_POS;
+    int catPreviousPos = catPos;
+
+    srand((unsigned int)time(NULL));
+
+    introAndGetName(catName, sizeof(catName));
+    reset(DELAY_INTRO);
+
+    while (1) {
+
+        // show status
+        showStatus(intimacy, soupCount);
+        mSleep(500);
+
+        // cat move 
+        printf("%s 이동: 집사와 친밀할수록 냄비 쪽으로 갈 확률이 높아집니다.\n", catName);
+        printf("주사위 눈이 %d 이상이면 냄비 쪽으로 이동합니다.\n", 6 - intimacy);
+
+        catPreviousPos = catPos;
+
+        int rollValue = rollDice();
+        if (rollValue >= 6 - intimacy) {
+            printf("냄비 쪽으로 움직입니다.\n");
+            if (catPos < BWL_PO)
+                catPos++;
+        }
+        else {
+            printf("집 쪽으로 움직입니다.\n");
+            if (catPos > HME_POS)
+                catPos--;
+        }
+        mSleep(500);
+
+        // check to make soup
+        int checkResult = checkCatPositionAndMakeSoup(catPos);
+
+        if (checkResult == 0) {
+            printf("%s은(는) 자신의 집에서 편안함을 느낍니다.\n", catName);
+        }
+        else if (checkResult > 0) {
+            soupCount++;
+            printf("%s이(가) %s를 만들었습니다!\n", catName,
+                checkResult == POTATO_SOUP ? "감자 수프" :
+                checkResult == MUSHROOM_SOUP ? "양송이 수프" : "브로콜리 수프");
+            printf("현재까지 만든 수프: %d개\n", soupCount);
+        }
+        mSleep(500);
+
+        // showRoom
+        showRoom(catPos, catPreviousPos);
+        mSleep(500);
+
+        // doAction
+        int action = getAction();
+        if (action == ACTION_NOTHING) {
+            printf("아무것도 하지 않습니다.\n");
+            printf("4 / 6의 확률로 친밀도가 떨어집니다.\n");
+            rollValue = rollDice();
+            if (rollValue <= 4) {
+                if (intimacy > INTIMACY_MIN)
+                    intimacy--;
+                printf("친밀도가 떨어집니다.\n");
+                printf("현재 친밀도 : %d\n", intimacy);
+            }
+            else {
+                printf("다행히 친밀도가 떨어지지 않았습니다.\n");
+                printf("현재 친밀도 : %d\n", intimacy);
+            }
+        }
+        else if (action == ACTION_SCRATCH) {
+            printf("쫀떡의 턱을 긁어주었습니다.\n");
+            printf("2 / 6의 확률로 친밀도가 높아집니다.\n");
+            rollValue = rollDice();
+            if (rollValue > 4) {
+                if (intimacy < INTIMACY_MAX)
+                    intimacy++;
+                printf("친밀도가 높아집니다.\n");
+                printf("현재 친밀도 : %d\n", intimacy);
+            }
+            else {
+                printf("친밀도는 그대로입니다.\n");
+                printf("현재 친밀도 : %d\n", intimacy);
+            }
+        }
+        reset(DELAY_TURN);
+    }
+}
+
+void reset(int milliSec) {
+    mSleep(milliSec);
+    system("cls");
+}
+void mSleep(int milliSec) {
+    Sleep(milliSec);
+}
+void introAndGetName(char* catName, int catNameLength) {
+    printf("****야옹이와 수프****\n\n");
+    printf("      /\\_/\\ \n");
+    printf(" /\\  / o o \\ \n");
+    printf("//\\\\ \\~(*)~/\n");
+    printf("`  \\/   ^ /\n");
+    printf("   | \\|| ||\n");
+    printf("   \\ '|| ||\n");
+    printf("    \\)()-())\n\n");
+    printf("야옹이의 이름을지어주세요: ");
+    scanf_s("%s", catName, catNameLength);
+    printf("야옹이의 이름은 %s입니다.\n\n", catName);
+}
+
+void showStatus(int intimacy, int soupCount)
+{
+    printf("==================== 현재 상태 ===================\n");
+    printf("현재까지 만든 수프 : %d개\n", soupCount);
+    printf("집사와의 관계(0~4) : %d\n", intimacy);
+    if (intimacy < INTIMACY_MIN || intimacy > INTIMACY_MAX) {
+        printf("ERROR: 허용된 STATUS 값을 벗어났습니다. STATUS:%d\n", intimacy);
+        exit(-1);
+    }
+    switch (intimacy)
+    {
+    case 0:
+        printf(" 곁에 오는것조차 싫어합니다.\n");
+        break;
+    case 1:
+        printf(" 간식 자판기 취급입니다.\n");
+        break;
+    case 2:
+        printf(" 그럭저럭 쓸 만한 집사입니다.\n");
+        break;
+    case 3:
+        printf(" 훌륭한 집사로 인정 받고 있습니다.\n");
+        break;
+    case 4:
+        printf(" 집사 껌딱지입니다.\n");
+        break;
+    }
+    printf("==================================================\n\n");
+}
+
+int getAction()
+{
+    int userAction = -1;
+    printf("어떤 상호작용을 하시겠습니까? 0. 아무것도 하지 않음 1. 긁어 주기\n");
+
+    do {
+        printf(">> ");
+        scanf_s("%d", &userAction);
+    } while (userAction != 0 && userAction != 1);
+
+    return userAction;
+}
+
+int rollDice()
+{
+    int random = rand() % 6 + 1;
+
+    printf("주사위를 굴립니다.또르륵...\n");
+    printf("%d이(가) 나왔습니다!\n", random);
+
+    return random;
+}
+
+void showRoom(int catPos, int catPreviousPos) {
+    printf("\n");
+    for (int i = 0; i < ROOM_WIDTH; i++)
+        printf("#");
+    printf("\n");
+
+    // home and bowl
+    for (int i = 0; i < ROOM_WIDTH; i++) {
+        if (i == HME_POS)
+            printf("H");
+        else if (i == BWL_PO)
+            printf("B");
+        else if (i == 0 || i == ROOM_WIDTH - 1)
+            printf("#");
+        else
+            printf(" ");
+    }
+    printf("\n");
+
+    // cat
+    for (int i = 0; i < ROOM_WIDTH; i++) {
+        if (catPos == i)
+            printf("C");
+        else if (catPreviousPos == i)
+            printf(".");
+        else if (i == 0 || i == ROOM_WIDTH - 1)
+            printf("#");
+        else
+            printf(" ");
+    }
+    printf("\n");
+
+    for (int i = 0; i < ROOM_WIDTH; i++)
+        printf("#");
+    printf("\n\n");
+}
+
+int checkCatPositionAndMakeSoup(int catPos)
+{
+    if (catPos == BWL_PO)
+        return rand() % 3 + 1;
+    else if (catPos == HME_POS)
+        return 0;
+    else
+        return -1;
+}
